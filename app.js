eaedc6ad58a528716aefa026056ed30ee68f5678
Oz SEL <ozsel75@hotmail.com>
2023-03-27 14:35:37 +0300

    Statik Dosyalar - Middleware Giriş

--- app.js
+++ app.js
@@ -1,7 +1,30 @@
+//npm init
+//npm i express
+//npm install --save-dev nodemon
+
 const express = require("express")
+const path = require("path")
 const app = express()
 
+
+
+const myLogger = (req, res, next) => {
+    console.log("middeleware log 1")
+    next();
+}
+
+const myLogger2 = (req, res, next) => {
+    console.log("middeleware log 2")
+    next();
+}
+// MIDDLEWARES
+app.use(express.static('public'))
+app.use(myLogger)
+app.use(myLogger2)
+
 app.get("/", (req, res)=>{
+
+    res.sendFile(path.resolve(__dirname,"temp/index.html"))
 
     const photo = {
         id: 1,
